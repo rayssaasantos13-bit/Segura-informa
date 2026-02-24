3d9d523aac70836fd0eae52ea8a30aae38655ca3
thaisdsantos10-glitch <thais.d.santos10@aluno.senai.br>
2026-02-24 11:10:21 -0300

    foi apagado linhas repetidas do css

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,8 @@
 function imprimir() {
     window.print();
 }
+function toggleMenu() {
+  const menu = document.getElementById("sideMenu");
+  menu.style.left = (menu.style.left === "0px") ? "-250px" : "0px";
+}
 
