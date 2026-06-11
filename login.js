7c6f8da6950a481f6e0440a882b38bc9b9575fef
thaisdsantos10-glitch <thais.d.santos10@aluno.senai.br>
2026-06-11 10:02:04 -0300

    tela de login finalizada

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
-const myFormLogin = document.getElementById('loginCliente');
+const myFormLogin = document.getElementById('loginUsuario');
 if (myFormLogin !=null){
 myFormLogin.addEventListener('submit', function (event) {
     // 1. Prevenir o recarregamento da página ao submeter form
     event.preventDefault();
 
-    fetch('https://localhost:7262/cliente/login', {
+    fetch('https://localhost:7175/Usuario/login', {
         method: 'POST', //Para outros métodos, basta alterar aqui. Obs: Delete remove a parte do body e headers, e no get é conforme todos os exemploes feitos na Unidade interação com API 
         credentials: 'include',
         headers: {
@@ -14,14 +14,15 @@ myFormLogin.addEventListener('submit', function (event) {
             nome: " ",
             email: document.getElementById("email").value,
             senha: document.getElementById("senha").value,
-            sexo: " "
+            cargo: " "
         }),
     }).then(response => {
+        console.log(response);
         if (response.status == 401) {
             alert("Email ou senha Incorretos!");
         } else {
             alert("Logado com suceeso");
-            window.location.href = "index.html";
+          window.location.href = "mapa de risco.html";
         }
     })
 
