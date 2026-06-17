efaa54ffc396359f2c4f40080c6d8a893ca2a576
thaisdsantos10-glitch <thais.d.santos10@aluno.senai.br>
2026-06-17 07:57:18 -0300

    tirei pq não é mais necessário

--- SeguraInforma/SeguraInforma/obj/Debug/net8.0/SeguraInforma.AssemblyInfo.cs
+++ SeguraInforma/SeguraInforma/obj/Debug/net8.0/SeguraInforma.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeguraInforma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+468ade1b08c4e3b495e8bce4ed5c10f05976aade")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+839ba3751eccbfb494c58ca4bf8cbdc2406b3ddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeguraInforma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeguraInforma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
