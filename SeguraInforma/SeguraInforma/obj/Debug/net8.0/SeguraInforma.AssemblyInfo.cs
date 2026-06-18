5857f458893c917259f98ed8af443d94e646eacf
anavsantos50-netizen <ana.v.santos50@aluno.senai.br>
2026-06-18 11:11:50 -0300

    atualizei

--- SeguraInforma/SeguraInforma/obj/Debug/net8.0/SeguraInforma.AssemblyInfo.cs
+++ SeguraInforma/SeguraInforma/obj/Debug/net8.0/SeguraInforma.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeguraInforma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92140bf821ab05c3425ae56cdcb3cd762e06f7be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8499fd1290573c234a67f002643c4692a910fa98")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeguraInforma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeguraInforma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
